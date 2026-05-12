5154b4ef4b728db800bb282f96f50cb033d4e9cb
Malik <melek.rebai@esprit.tn>
2026-05-12 13:57:53 +0100

    empty master branch

--- views/back/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="admin-footer">
-    <span>Smart Nutrition Back Office</span>
-    <span><?= date('Y') ?></span>
-</footer>
-<script src="<?= htmlspecialchars($assetBase) ?>/js/backoffice.js" defer></script>
-</body>
-</html>
--- views/front/stats/index.php
+++
@@ -1,1 +0,0 @@
-<?php require __DIR__ . '/../aliments/stats.php'; ?>
